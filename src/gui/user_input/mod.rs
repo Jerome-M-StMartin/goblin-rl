d3974e5aa179e7f0f9aefbe2502e94b4bc7998c7
Jerome M. St.Martin <jeromemst.martin@gmail.com>
2022-05-30 18:07:49 -0700

    fleshed out custom error

--- src/gui/user_input/mod.rs
+++ src/gui/user_input/mod.rs
@@ -1,8 +1,10 @@
 //Jerome M. St.Martin
 //May, 2022
 
+use crossterm::event::{poll, Event, KeyCode, KeyEvent, KeyModifiers};
 use std::time::Duration;
-use crossterm::event::{poll, Event, KeyEvent, KeyCode, KeyModifiers};
+
+use super::super::error::Gremlin;
 
 //-------------------------------------------
 //--------------- CONTROLLER ----------------
@@ -15,12 +17,8 @@ pub struct UserInput {
 
 impl UserInput {
     pub fn new() -> Self {
-        UserInput {
-            event: None,
-        }
+        UserInput { event: None }
     }
-
-    pub fn tick(&mut self) {}
 
     /*fn blocking_read() -> crossterm::Result<Event> {
         /*match read()? {
@@ -31,15 +29,26 @@ impl UserInput {
 
     //If written correctly: Does Not Block
     fn read() -> crossterm::Result<Event> {
-
         if poll(Duration::from_millis(500))? {
-
             match crossterm::event::read()? {
                 event => return Ok(event),
             }
-
-        } else { /* poll timeout expired */ };
+        } else { /* poll timeout expired */
+        };
 
         Ok(Event::Key(KeyEvent::new(KeyCode::Null, KeyModifiers::NONE)))
+    }
+
+    pub fn tick(&mut self) -> Result<(), Gremlin> {
+        let event = Self::read()?;
+        self.store(event);
+
+        //TODO
+
+        Ok(())
+    }
+
+    fn store(&mut self, e: Event) {
+        self.event = Some(e);
     }
 }
